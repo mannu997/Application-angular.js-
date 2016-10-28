db401a87b6871c6d74ea59e2bcdcf972f3b452b0
Manmohan <mghanghas1994@gmail.com>
2016-10-28 23:24:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
 (function(){
 	var app = angular.module("add-player",[]);
 	
-	var data=[{
-		name:"manmohan",
-		score:"20",
-		deleted:false,
-	}];
+	var data=[];
 	
 	app.controller("PlayerController",function(){
 		this.entries=data;
 	});
 	
-	app.controller("FormController",function(){
-		this.entry={};
+	app.controller("EditScore",function($scope){
+		this.editScore=$scope.score;
+		this.edit=function(player){
+		player[score]=this.editScore;
+		};
+	});
+	
+	app.controller("FormController",function($scope){
+		this.entry={name:$scope.name,
+                    score:$scope.score,
+                    deleted:false					
+		};
 		
-		this.addEntries=function(list){
-			this.entry[deleted]=false;
-			list.push(this.entry);
+		this.addEntries=function(){
+		    data.push(this.entry);
 			this.entry={};
 		};
 	});
