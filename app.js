340a288eaca4a3447657192398ff41f1818360ad
Manmohan <mghanghas1994@gmail.com>
2016-11-02 07:26:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,60 @@
 (function(){
 	var app = angular.module("add-player",[]);
 	
-	var data=[];
-	
-	app.controller("PlayerController",function(){
-		this.entries=data;
-	});
-	
-	app.controller("EditScore",function($scope){
-		this.editScore=$scope.score;
-		this.edit=function(player){
-		player[score]=this.editScore;
-		};
-	});
-	
-	app.controller("FormController",function($scope){
-		this.entry={name:$scope.name,
-                    score:$scope.score,
-                    deleted:false					
+	app.controller("LeaderboardController",function($scope){
+		$scope.players=[];
+		$scope.showbuttons=false;
+		
+		$scope.createPlayer=function(name,score){
+			if ($scope.showbuttons==false)
+				$scope.showbuttons=true;
+			var player = {
+				"name":name,
+				"score":score
+			}
+			$scope.players.push(player);
+			$scope.name="";
+			$scope.score=null;
 		};
 		
-		this.addEntries=function(){
-		    data.push(this.entry);
-			this.entry={};
+		$scope.editPlayer=function(player,score){
+			var index=$scope.players.indexOf(player);
+			$scope.players[index]["score"]=score;
+			
+			
 		};
+		
+		$scope.deletePlayer=function(player){
+			var index=$scope.players.indexOf(player);
+			$scope.players.splice(index,1);
+			if ($scope.players.length==0 && $scope.showbuttons==true)
+				$scope.showbuttons=false;
+		};
+		 
+		$scope.sortbyname=function(){
+			var compare=function(a,b){
+				if (a.name<b.name)
+					return -1;
+				if (a.name>b.name)
+                    return 1;
+                return 0					
+			};
+		    $scope.players.sort(compare);
+		}
+		
+		$scope.sortbyscore=function(){
+			var compare=function(a,b){
+				if (a.score<b.score)
+					return -1;
+				if (a.score>b.score)
+                    return 1;
+                return 0					
+			};
+		    $scope.players.sort(compare);
+		}
+		
+		
 	});
+	
     
 })();
