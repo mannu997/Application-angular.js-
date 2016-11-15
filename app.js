390d883b7e145e0d71d2bb1b7e288af67e982552
manmohan <mghanghas1994@gmail.com>
2016-11-15 20:26:26 +0530

    submit data to database,display data to frontend

--- app.js
+++ app.js
@@ -1,27 +1,34 @@
 (function(){
 	var app = angular.module("add-player",[]);
 	
-	app.controller("LeaderboardController",function($scope){
+	app.controller("LeaderboardController",['$http','$scope',function($http,$scope){
 		$scope.players=[];
 		$scope.showbuttons=false;
-		
-		$scope.createPlayer=function(name,score){
-			if ($scope.showbuttons==false)
-				$scope.showbuttons=true;
-			var player = {
-				"name":name,
-				"score":score
-			}
-			$scope.players.push(player);
-			$scope.name="";
-			$scope.score=null;
-		};
+        
+        $scope.load = function(){
+             $http.get(window.location.href+"display.php").success(function(data){
+                    $scope.players = data;
+             });
+         }
+
+	    $scope.request = function(){
+               var response=$http({
+                    method: "post",
+                    url: window.location.href+"/submit.php",
+                    data: {
+                           name: $scope.name,
+                           score: $scope.score
+                           },
+                    headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+            });
+            response.success(function (data) {
+                    document.getElementById("message").textContent = "data submitted"
+            });
+	    };
 		
 		$scope.editPlayer=function(player,score){
 			var index=$scope.players.indexOf(player);
 			$scope.players[index]["score"]=score;
-			
-			
 		};
 		
 		$scope.deletePlayer=function(player){
@@ -54,7 +61,7 @@
 		}
 		
 		
-	});
+	}]);
 	
     
 })();
