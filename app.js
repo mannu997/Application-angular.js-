9c1f6ab1d3dd89f23362bf58177753b15ac83b25
Manmohan <mghanghas1994@gmail.com>
2016-11-16 20:47:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,63 +1,77 @@
 (function(){
 	var app = angular.module("add-player",[]);
 	
-	app.controller("LeaderboardController",['$http','$scope',function($http,$scope){
-		$scope.players=[];
-		$scope.showbuttons=false;
+    app.controller("LeaderboardController",['$http','$scope',function($http,$scope){
+		    $scope.players=[];
+		    $scope.showbuttons=true;
         
         $scope.load = function(){
-             $http.get(window.location.href+"display.php").success(function(data){
-                    $scope.players = data;
-             });
-         }
-
-	    $scope.request = function(){
-               var response=$http({
-                    method: "post",
-                    url: window.location.href+"/submit.php",
-                    data: {
-                           name: $scope.name,
-                           score: $scope.score
-                           },
-                    headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+            $http.get(window.location.href+"/display.php").success(function(response){
+                $scope.players = response;
             });
-            response.success(function (data) {
-                    document.getElementById("message").textContent = "data submitted"
-            });
-	    };
-		
-		$scope.editPlayer=function(player,score){
-			var index=$scope.players.indexOf(player);
-			$scope.players[index]["score"]=score;
-		};
-		
-		$scope.deletePlayer=function(eyed){
-		
+        }
 
+	      $scope.request = function(){
+            var response=$http({
+                method: "post",
+                url: window.location.href+"/submit.php",
+                data: {
+                    name: $scope.name,
+                    score: $scope.score
+                },
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+            });
+            $scope.load();
+            $scope.name="";
+            $scope.score=null;
+	      };
 		
-		};
-		 
-		$scope.sortbyname=function(){
-			var compare=function(a,b){
-				if (a.name<b.name)
-					return -1;
-				if (a.name>b.name)
+		   $scope.editPlayer=function(player,score){
+			      var response=$http({
+                method: "post",
+                url: window.location.href+"/edit.php",
+                data: {
+                    id: player.id,
+                    score: score
+                },
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+            });
+		        $scope.load();
+        };
+		
+        $scope.deletePlayer=function(player){
+            var response =  $http({
+                method: "post",
+                url: window.location.href+"/delete.php",
+                data: {
+                    id: player.id
+                },
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+            });
+            $scope.load();
+        };		
+        
+		    $scope.sortbyname=function(){
+			      var compare=function(a,b){
+				        if (a.name<b.name)
+					          return -1;
+				        if (a.name>b.name)
                     return 1;
                 return 0					
-			};
-		    $scope.players.sort(compare);
-		}
+		      	};
+		        $scope.players.sort(compare);
+		    }
 		
-		$scope.sortbyscore=function(){
-			var compare=function(a,b){
-				if (a.score<b.score)
-					return -1;
-				if (a.score>b.score)
+		    $scope.sortbyscore=function(){
+			     var compare=function(a,b){
+				        if (a.score<b.score)
+				           	return -1;
+				        if (a.score>b.score)
                     return 1;
                 return 0					
-			};
-		    $scope.players.sort(compare);
-		}
+			      };
+		        $scope.players.sort(compare);
+		  }
 		
 		
 	}]);
